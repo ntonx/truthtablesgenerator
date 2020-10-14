410d9ebe507156803f4e494cb590c9b350e303e9
Antonio Nicolas ideapad <ntonx9@gmail.com>
2020-10-14 13:28:45 -0500

    adding 1 filter in index path

--- src/main/java/com/example/logicproject/GeneratorService.java
+++ src/main/java/com/example/logicproject/GeneratorService.java
@@ -83,6 +83,116 @@ public class GeneratorService {
 			}
 	    	
    	 		}
+	    
+	    int nbrCombinaisons1 = (int) Math.pow(2, nbrVariables-1);
+	    int nbrCombinaisons2 = (int) Math.pow(2, nbrVariables-2);
+	    
+	    int completewith1or0 [][] = new int [nbrCombinaisons1][nbrVariables+1];
+	    int completewith1and0 [][] = new int [nbrCombinaisons2][nbrVariables+1];
+	       
+	    if(datos.contains("1")&&!datos.contains("0")) {
+	    	for(int p=0;p<nbrCombinaisons/2;p++) {    		
+	    		for(int k=0;k<=nbrVariables;k++) {
+	    				completewith1or0[p][k]=complete[p][k];
+	    		}
+	    	}    	 	
+	    	return completewith1or0; 	
+	    }else if(datos.contains("0")&&!datos.contains("1")) {
+	    	for(int p=0;p<nbrCombinaisons/2;p++) {    		
+	    		for(int k=0;k<=nbrVariables;k++) {
+	    				completewith1or0[p][k]=complete[p][k];
+	    		}
+	    	}    	    	
+	    	return completewith1or0; 	
+	    }else if(datos.contains("1")&&datos.contains("0")) {
+	    	for(int p=0;p<nbrCombinaisons/4;p++) {    		
+	    		for(int k=0;k<=nbrVariables;k++) {
+	    			completewith1and0[p][k]=complete[p][k];
+	    		}
+	    	}    	   	
+	    	return completewith1and0; 	
+	    }
+	    return complete;     
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int[][] getValuesFromExpression2(int nbrVariables, String expresion,List<String>datos) {
+		//if there is a biconditional remove the second element > to avoid conflicts at reading
+		expresion = expresion.replaceAll("<>", "<");
+		
+		List<String> mycadena = getPostFixString(expresion);
+		System.out.println("MI EXPRESION POSFIJA:"+mycadena);
+	    
+		int nbrCombinaisons = (int) Math.pow(2, nbrVariables);
+	    Boolean[] boolArray = new Boolean[nbrVariables+1];
+	    int[] intArray = new int[nbrVariables+1];
+	    
+	    int complete [][] = new int [nbrCombinaisons][nbrVariables+1]; 
+	    boolean tt [][] = new boolean [nbrCombinaisons][nbrVariables+1]; 
+	    
+	    for (int j = 0; j < nbrCombinaisons; j++) {             
+	        String    tempStr  = String.format("%"+nbrVariables+"s", Integer.toBinaryString(j)).replace(" ", "0");
+	        boolean[] tempBool = new boolean[tempStr.length()+1];
+	        boolean total = tempStr.charAt(0)=='1';
+	        for(int i=0; i<tempStr.length(); i++){
+	            tempBool[i]= tempStr.charAt(i)=='1';
+	            if(i>0){	
+	            	total= false;
+	        //        total = total && tempBool[i];  //table for logical AND change operator to || for OR or ^ for XOR
+	            }
+	        }
+	        tempBool[tempStr.length()] = total;
+	        tt[j] = tempBool;
+	    }
+
+	    int posicionuno=-1;
+	    int posicioncero=-1;
+	    
+	    for (int j = 0; j < nbrCombinaisons; j++) { 
+	    	for (int i = 0; i <boolArray.length ; i++) {
+   		// 			boolArray[i]= tt[j][i];
+   		 		if(tt[j][i]==true) {
+   		 			intArray[i]= 1;
+   		 		}else {
+   		 			intArray[i]= 0;
+   		 			
+   		 		}
+   		 		
+	   		 	if(datos.contains("1")) {
+	   		 		posicionuno = datos.indexOf("1");
+		 			if(i==posicionuno) {
+		 				intArray[i]=1;
+		 			}
+			    }
+	   		 	if(datos.contains("0")) {
+	   		 		posicioncero = datos.indexOf("0");
+			    	if(i==posicioncero) {
+			    		intArray[i]=0;
+			    	}
+			    }	
+	    	}
+	    	
+	    	int[]copia = intArray;
+	    	List<String> mycadenacopia=new ArrayList<>(mycadena);
+	    	int valor = calculatePerRow(copia,mycadenacopia,datos);
+	    	//save in the final colum of each row the result of processing the row with posfix  
+	    	intArray [nbrVariables] = valor;
+	    	
+	    	for (int i = 0; i < intArray.length; i++) {
+	    		//build the array that contains all the processing data
+				complete[j][i] = intArray[i];
+			}
+	    	
+   	 		}
+	   	    
 	    return complete;     
 	}
 	
--- src/main/java/com/example/logicproject/LogicController.java
+++ src/main/java/com/example/logicproject/LogicController.java
@@ -21,7 +21,7 @@ public class LogicController {
 	 GeneratorService truthTablesGenerator;
 	 BuilHTMLService htmlBuilder;
 	
-	@PostMapping(value="/",produces = MediaType.TEXT_HTML_VALUE)
+	@PostMapping(value="/index",produces = MediaType.TEXT_HTML_VALUE)
 	@ResponseBody
 	public String statementSubmit(@ModelAttribute Statement mychar, Model model) {
 		model.addAttribute("mystatement", mychar);
@@ -31,9 +31,6 @@ public class LogicController {
 		if(expresion.isEmpty()) {
 			return "Write a statement on the box!!!";
 		}
-		
-	//	String	expresion="~(p+q)>(p>r)";//"a*(b+c)";//;"p>q";
-	//	String expresion="(a*b)>c";
 		
 		tokenService = new TokenizerService();
 		List<String>datos = tokenService.getOperands(expresion);
@@ -71,15 +68,98 @@ public class LogicController {
 	    }      
 		page = page+aux2;
 		
+		
+		
+		if(!(datos.contains("1"))&&!(datos.contains("0"))) {    
+			for(int i=0;i<(datos.size()*datos.size())-1;i++) {
+				response.add(res[i][datos.size()]);
+				System.out.println(res[i][datos.size()*datos.size()-1]);
+			}
+			if(datos.size()==1) {
+				for(int i=0;i<=datos.size();i++) {
+					response.add(res[i][datos.size()]);
+				}
+			}
+		}else if(datos.contains("1")&&!(datos.contains("0"))||!datos.contains("1")&&(datos.contains("0"))) {
+			
+			for(int i=0;i<(datos.size());i++) {
+				response.add(res[i][datos.size()]);
+			}
+			if(datos.size()==1) {
+				for(int i=0;i<=datos.size();i++) {
+					response.add(res[i][datos.size()]);
+				}
+			}
+		}else if(datos.contains("1")&&(datos.contains("0"))) {
+			for(int i=0;i<(datos.size());i++) {
+				response.add(res[i][datos.size()]);
+			}
+			if(datos.size()==1) {
+				for(int i=0;i<=datos.size();i++) {
+					response.add(res[i][datos.size()]);
+				}
+			}
+		}
+		
+	  return page+htmlBuilder.getEnd(response);
+	}
+	
+	
+	@PostMapping(value="/",produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String statementSubmit2(@ModelAttribute Statement mychar, Model model) {
+		model.addAttribute("mystatement", mychar);
+		
+		String expresion = mychar.content;
+		
+		if(expresion.isEmpty()) {
+			return "Write a statement on the box!!!";
+		}
+		
+		tokenService = new TokenizerService();
+		List<String>datos = tokenService.getOperands(expresion);
+		
+		truthTablesGenerator= new GeneratorService();
+		
+		int[][] res = truthTablesGenerator.getValuesFromExpression2(datos.size(),expresion,datos);
+		
+		htmlBuilder=new BuilHTMLService();
+		
+		String page = htmlBuilder.getHead();
+
+		String m = "<th scope=\"col\">";
+		String m2= "</th>";
+		for(int i = 0;i<datos.size();i++) {
+			String aux = "";
+			aux = m+datos.get(i)+m2;
+			page=page+aux;
+		}
+		
+		page=page+m+expresion+m2+htmlBuilder.getMiddle();
+		 
+		String m4 = "<td>";
+		String m5 = "</td>";
+		String aux2 = "";
+		String m6="</tr>";
+		ArrayList<Integer> response = new ArrayList<>();
+		for (int[] row : res) {            
+	        for (int c : row) { 
+	        	aux2=aux2+m4+c+m5;
+//	        	System.out.print(c + "\t");        
+	        }
+	        aux2 = aux2+m6;
+//	        System.out.println();
+	    }      
+		page = page+aux2;
+		   
 		for(int i=0;i<(datos.size()*datos.size())-1;i++) {
-			response.add(res[i][datos.size()]);
+			response.add(res[i][datos.size()]);		
 		}
 		if(datos.size()==1) {
 			for(int i=0;i<=datos.size();i++) {
 				response.add(res[i][datos.size()]);
 			}
-		}
-		
+		}	
 	  return page+htmlBuilder.getEnd(response);
 	}
 	
@@ -89,6 +169,12 @@ public class LogicController {
 	public String statementForm(Model model) {
 		model.addAttribute("mystatement", new Statement());
 		return "index";
+	}
+	
+	@GetMapping("/index")
+	public String statementForm1(Model model) {
+		model.addAttribute("mystatement", new Statement());
+		return "index2";
 	}
 		
 	
